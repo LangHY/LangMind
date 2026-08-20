dc9798d0c33158463fafb09b5cbe254c118bdf7a
LANG <lang@LANGdeMacBook-Pro.local>
2026-08-20 21:03:58 +0800

    code the GQA(1)

--- notes/GQA.py
+++ notes/GQA.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 
 
 print("=====nn.Dropout=====")
@@ -52,3 +53,67 @@ print(torch.triu(x, diagonal=-1))
 # 将对角线下移一格
 print(torch.triu(x, diagonal=1))
 # 将对角线上移一格
+
+print("=====GQA实现=====")
+
+X = torch.randn(2, 4, 8) #输入 4 个 token，每个 token 8 维
+w_Q = torch.randn(8, 8)
+w_K = torch.randn(8, 8)
+w_V = torch.randn(8, 8)
+
+
+def Attention(X:torch.tensor, w_Q:torch.tensor, w_K:torch.tensor, w_V:torch.tensor, nums_head:int):
+    # 计算输入 token 的 QKV
+    Q = torch.tensor(X @ w_Q)
+    K = torch.tensor(X @ w_K)
+    V = torch.tensor(X @ w_V)
+    print(f"Q_shape = {Q.shape}")
+    print(f"K_shape = {K.shape}")
+    print(f"V_shape = {V.shape}")
+    print(f"K^T = {K.transpose(-1, -2).shape}")
+
+
+    seq = (Q.shape[-2]) # token数量
+    batch = Q.shape[0] #batch数
+    d_token = X.shape[-1] #token的维度（用于计算注意力头分得的维度）
+    d_K = list(K.shape)[-1] #K的维度（ 用于计算 attention）
+    #依据注意力头数计算每个注意力头分得的维度（Head QKV 的维度）
+    d_Head = int(d_token / nums_head)
+
+
+    # 依据注意力头数拆 token
+    Q = Q.view(batch, seq, nums_head, d_Head)
+    K = K.view(batch, seq, nums_head, d_Head)
+    # [batch, seq, nums_head, head_dim]
+    # [2,     4,   2,      4]
+    print(Q.shape, K.shape)
+
+    Q = Q.transpose(1, 2)
+    K = K.transpose(1, 2)
+    print(Q.shape, K.shape)
+    # [batch, nums_head, seq, head_dim]
+    # PyTorch 的批量矩阵乘法：
+    # 默认会把：
+    # 最后两个维度当矩阵乘法维度
+    # 前面的维度当 batch 维度。
+    # 所以我们希望：
+
+    # 前面的维度：
+    # batch
+    # head
+
+    # 最后两个：
+    # token
+    # dim
+
+    score = Q @ (K.transpose(-1, -2))
+    print(f"score = {score.shape}")
+    # [batch, nums_head, query token, key token]
+    score = score / math.sqrt(d_K)
+
+    attention = (torch.softmax(score, dim=-1)) @ V
+    return attention
+
+
+result = Attention(X, w_Q, w_K, w_V, nums_head=2)
+print(result)
